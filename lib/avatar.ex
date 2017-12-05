ba2f5c82124fead7ba8f9cec76e98c902cd6ac6e
Steven Xu <stevenxu@Stevens-iMac.local>
2017-12-05 16:43:16 +0800

    add effect handling

--- lib/avatar.ex
+++ lib/avatar.ex
@@ -87,13 +87,59 @@ defmodule Avatar do
   end
 
   defp handle_result({:notify, events, changed}, {id, session, data}) do
-    # {events, changed} = apply_rules({events, changed}, {id, data})
+    {events, changed} = apply_rules({events, changed}, {id, data})
     GenServer.cast(session, {:notify, events})
     {:noreply, {id, session, data |> merge(changed)}}
   end
 
+  defp handle_result({:resolve, context, effects}, {id, session, data}) do
+    # apply partial resolved changes
+    {resolved, data} = resolved(context, {id, data})
+
+    {events, data} =
+      List.wrap(effects)
+      |> Enum.flat_map_reduce(data, fn effect, data ->
+        {events, changed} = resolve(effect, {id, context, data})
+        {List.wrap(events), data |> merge(changed)}
+      end)
+
+    GenServer.cast(session, {:notify, resolved ++ events})
+    {:noreply, {id, session, data}}
+  end
+
   defp handle_result(:ok, state) do
     {:noreply, state}
+  end
+
+  defp resolved(context, {id, data}) when is_map(context) do
+    events = context |> Map.get(:events) |> List.wrap
+    changed = context |> Map.get(:changed, %{})
+
+    {events, changed} = apply_rules({events, changed}, {id, data})
+    {events, merge(data, changed)}
+  end
+
+  defp resolved(_context, {_, data}) do
+    {[], data}
+  end
+
+  # TODO: check points => attrbutes => stats
+  # TODO: build a pipeline based on changed props, then apply
+  defp apply_rules({events, changed}, {id, data}) do
+    {events, changed} =
+      changed |> Enum.reduce({List.wrap(events), %{}}, fn change, {events, changed} ->
+        change = Map.new([change])
+        {new_events, new_changed} = Rules.apply_rule(change, {id, data})
+        {events ++ List.wrap(new_events), changed |> Map.merge(new_changed)}
+      end)
+
+    {events, changed} =
+      events |> Enum.reduce({events, changed}, fn event, {events, changed} ->
+      {new_events, new_changed} = Rules.apply_rule(event, {id, data})
+      {events ++ List.wrap(new_events), changed |> Map.merge(new_changed)}
+    end)
+
+    {events, changed}
   end
 
   defp merge(data, nil) do
@@ -108,6 +154,10 @@ defmodule Avatar do
     changed |> Enum.reduce(data, fn
       {path, value}, data -> data |> put_in(path, value)
     end)
+  end
+
+  defp resolve(effect, {id, context, data}) do
+    Effect.resolve(effect, {id, context, data})
   end
 
   defp client_info({id, data}) do
--- lib/effect.ex
+++ lib/effect.ex
@@ -26,4 +26,79 @@ defmodule Effect do
   def from_cost_rewards(cost, rewards) do
     Enum.concat from_cost(cost), from_rewards(rewards)
   end
+
+  def resolve({:gain, :item, item}, {id, _context, %{bag: bag}}) do
+    {cell, bag} = bag |> Inventory.store(item)
+    event = {{:bag, :gain}, id, %{cell => item}}
+
+    {event, %{bag: bag}}
+  end
+
+  def resolve({:gain, {:item, item_id}, count}, {id, _context, %{bag: bag}}) do
+    {cells, bag} = bag |> Inventory.stack(item_id, count)
+
+    {{{:bag, :gain}, id, Map.new(cells)}, %{bag: bag}}
+  end
+
+  def resolve({:gain, {mod, prop}, amount}, {id, _context, data}) do
+    with group when not is_nil(group) <- Map.get(data, mod),
+      original when not is_nil(original) <- Map.get(group, prop),
+      current = original + amount
+    do
+      {{:prop_changed, id, %{mod => %{prop => current}}}, %{mod => %{group | prop => current}}}
+    else
+      _ -> {[], %{}}
+    end
+  end
+
+  def resolve({:gain, prop, amount}, {id, _context, %{currencies: currencies, points: points}}) do
+    if Map.has_key?(currencies, prop) do
+      current = Map.get(currencies, prop) + amount
+      {{:prop_changed, id, %{prop => current}}, %{currencies: %{currencies | prop => current}}}
+    else
+      if Map.has_key?(points, prop) do
+        current = Map.get(points, prop) + amount
+        {{:prop_changed, id, %{prop => current}}, %{points: %{points | prop => current}}}
+      end
+    end
+  end
+
+  def resolve({:lost, {:bag, index}, count}, {id, _context, %{bag: bag}}) do
+    {lost, bag} = Inventory.pop_some_at(bag, index, count)
+    {{{:bag, :lost}, id, %{index => lost}}, %{bag: bag}}
+  end
+
+  def resolve({:lost, {:item, item_id}, count}, {id, _context, %{bag: bag}}) do
+    {^count, poped, bag} = Inventory.pop_some(bag, item_id, count)
+    {{{:bag, :lost}, id, Map.new(poped)}, %{bag: bag}}
+  end
+
+  def resolve({:lost, {mod, prop}, amount}, {id, _context, data}) do
+    with group when not is_nil(group) <- Map.get(data, mod),
+      original when not is_nil(original) <- Map.get(group, prop),
+      current = original - amount
+    do
+      {{:prop_changed, id, %{mod => %{prop => current}}}, %{mod => %{group | prop => current}}}
+    else
+      _ -> {[], %{}}
+    end
+  end
+
+  def resolve({:lost, prop, amount}, {id, _context, %{currencies: currencies, points: points}}) do
+    if Map.has_key?(currencies, prop) do
+      current = Map.get(currencies, prop) - amount
+      {{:prop_changed, id, %{prop => current}}, %{currencies: %{currencies | prop => current}}}
+    else
+      if Map.has_key?(points, prop) do
+        current = Map.get(points, prop) - amount
+        {{:prop_changed, id, %{prop => current}}, %{points: %{points | prop => current}}}
+      end
+    end
+  end
+
+  def resolve(effect, {_id, _context, _data}) do
+    IO.puts "unknown effect #{inspect effect}"
+
+    {[], %{}}
+  end
 end
